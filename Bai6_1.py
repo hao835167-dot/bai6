1355fc676239b3491fb27f746275dca9259151d9
hao835167-dot <hao835167@gmail.com>
2025-12-01 09:06:05 +0700

    Update Bai6_1.py

--- Bai6_1.py
+++ Bai6_1.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 class Circle:
     def __init__(self, r):
         self.radius = r

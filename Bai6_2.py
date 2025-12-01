fcb9d600292dbd1af9bf7d2f4b8993bfd87fbcda
hao835167-dot <hao835167@gmail.com>
2025-12-01 09:06:19 +0700

    Update Bai6_2.py

--- Bai6_2.py
+++ Bai6_2.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 class Hinhchunhat:
     def __init__(self, dai, rong):
         self.chieu_dai = dai
